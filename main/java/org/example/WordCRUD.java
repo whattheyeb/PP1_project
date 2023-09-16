eb015fb061745812a9b2fcab7e1849ad09cf61ae
whattheyeb <overtokki08@gmail.com>
2023-09-16 13:40:47 +0900

    단어 저장할때 빈칸 없애기

--- main/java/org/example/WordCRUD.java
+++ main/java/org/example/WordCRUD.java
@@ -22,7 +22,7 @@ public class WordCRUD implements ICRUD {
 
     @Override
     public Object add() {
-        System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
+        System.out.print("=> 난이도(1,2,3) & 새 단어 입력 :");
         int level = s.nextInt();
         String word = s.nextLine();
 
